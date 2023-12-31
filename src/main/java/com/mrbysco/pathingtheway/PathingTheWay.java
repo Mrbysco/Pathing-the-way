aea43c58d0bbe93d56054463ba3f93b7f770891b
Mrbysco <Mrbysco@users.noreply.github.com>
2023-12-31 04:11:48 +0100

    Port to 1.20.4

--- src/main/java/com/mrbysco/pathingtheway/PathingTheWay.java
+++ src/main/java/com/mrbysco/pathingtheway/PathingTheWay.java
@@ -3,12 +3,11 @@ package com.mrbysco.pathingtheway;
 import com.mojang.logging.LogUtils;
 import com.mrbysco.pathingtheway.config.PathingConfig;
 import com.mrbysco.pathingtheway.handler.PathHandler;
-import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 
 @Mod(PathingTheWay.MOD_ID)
@@ -16,8 +15,7 @@ public class PathingTheWay {
 	public static final String MOD_ID = "pathingtheway";
 	public static final Logger LOGGER = LogUtils.getLogger();
 
-	public PathingTheWay() {
-		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+	public PathingTheWay(IEventBus eventBus) {
 		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, PathingConfig.serverSpec);
 		eventBus.register(PathingConfig.class);
 
